40cfeddd84e826cc3e5e13ede27640be7e853bc9
yjh <eltonchan@live.com>
2018-02-10 20:41:00 +0800

    bluebird

---
+++ buleBird.js
@@ -0,0 +1,41 @@
+const Promise = require('bluebird');
+const { info } = console;
+
+const delay = timeout => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(timeout);
+    }, timeout);
+});
+
+let arr = [
+    delay(1000),
+    delay(2000),
+    delay(3000),
+];
+
+let mapArr = [1000, 2000, 3000];
+// 找到最快执行的2个
+Promise.some(arr, 2).spread((f, s) => {
+    info('f', f); // 1000
+    info('s', s); // 2000
+});
+
+// 把数组封装成promise对象 ==> Promise.all
+let map = Promise.map(mapArr, num => {
+    return Promise.resolve(num / 1000);
+}).then(res => {
+    info('res1', res); // [1, 2, 3]
+    return res.join('');
+});
+
+info('map', map); // promise 对象
+map.then(res => {
+    info('map', res); // map 123
+});
+
+Promise.map(mapArr, num => {
+    return delay(num).then(res => res);
+}).then(res => {
+    info('res2', res); // [1000, 2000, 3000]
+});
+
