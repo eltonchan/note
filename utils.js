e6699ecdbd40271f4caa706c82a857ea57561fb2
huasheng <eltonchan@live.com>
2018-05-30 15:23:30 +0800

    change

--- utils.js
+++ utils.js
@@ -10,6 +10,23 @@ const loadImg = url => new Promise((resolve, reject) => {
     }
 });
 
+const funDownload = (content, filename) => {
+    // 创建隐藏的可下载链接
+    var eleLink = document.createElement('a');
+    eleLink.download = filename;
+    eleLink.style.display = 'none';
+    // 字符内容转变成blob地址
+    var blob = new Blob([content]);
+    eleLink.href = URL.createObjectURL(blob);
+    // 触发点击
+    document.body.appendChild(eleLink);
+    eleLink.click();
+    // 然后移除
+    document.body.removeChild(eleLink);
+};
+
+
 window.utils = {
     loadImg,
+    funDownload,
 };
